490b4fe3c92e676989d9b0e9997f90f0ba413828
penzrgb <rodger.benham@gmail.com>
2013-08-20 20:08:23 +1000

    Naive initial structure for the model
    The model will be broken into its own assembly to properly encapsulate
    the classes that should be only interacted with via the facade.

---
+++ YatttgModel/YatttgModel/CellManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YatttgModel
+{
+    class CellManager
+    {
+        private Cell[,] grid_;
+
+        public CellManager()
+        {
+            grid_ = new Cell[3,3];
+        }
+    }
+}
---
+++ YatttgModel/YatttgModel/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YatttgModel
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public Marker Marker { get; set; }
+
+        public Player(string name, Marker marker)
+        {
+            this.Name = name;
+            this.Marker = marker;
+        }
+    }
+}
